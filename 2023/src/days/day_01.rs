3b3865e4fb6bb711b9c1d1f6cebd10a32baab738
legoraft <50689727+legoraft@users.noreply.github.com>
2023-11-30 16:40:22 +0100

    Added new files and structure

---
+++ 2023/src/days/day_01.rs
@@ -0,0 +1,3 @@
+pub fn main() -> String {
+    "Hello, world!".to_string()
+}
---
+++ 2023/src/tests.rs
@@ -0,0 +1,8 @@
+use super::*;
+
+#[test]
+pub fn day_01() {
+    let input = "Hello, world!";
+
+    assert_eq!(input, days::day_01::main())
+}
