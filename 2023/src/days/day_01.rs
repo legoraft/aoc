367f2e29ae93021be67799a572ccce10c22a36f0
legoraft <50689727+legoraft@users.noreply.github.com>
2023-12-01 08:54:42 +0100

    Completed part 2 with reddit help

--- 2023/src/days/day_01.rs
+++ 2023/src/days/day_01.rs
@@ -1,28 +1,51 @@
 pub fn main() {
     let calibration_value = include_str!("../inputs/day_01.txt");
-    println!("{:?}", part_one(calibration_value));
+    println!("Part 1: {:?}\nPart 2: {:?}", part_one(calibration_value), part_two(calibration_value));
 }
 
 pub fn part_one(calibration_value: &str) -> u32 {
     let lines: Vec<&str> = calibration_value.lines().collect();
     let mut values: Vec<Vec<u32>> = Vec::new();
-    let mut sum: u32 = 0;
 
     for line in lines {
         let numbers: Vec<u32> = line.chars().filter_map(|num| num.to_digit(10)).collect();
         values.push(numbers);
     }
 
-    for numbers in values {
-        let digit = numbers[0].to_string() + &*numbers[numbers.len() - 1].to_string();
-        sum += digit.parse::<u32>().unwrap();
-    }
-
+    let sum = calculate_sum(values);
     sum
 }
 
 pub fn part_two(calibration_value: &str) -> u32 {
+    let lines: Vec<&str> = calibration_value.lines().collect();
+    let mut values: Vec<Vec<u32>> = Vec::new();
 
+    for line in lines {
+        let numbers: Vec<u32> = line.to_string()
+            .replace("one", "one1one")
+            .replace("two", "two2two")
+            .replace("three", "three3three")
+            .replace("four", "four4four")
+            .replace("five", "five5five")
+            .replace("six", "six6six")
+            .replace("seven", "seven7seven")
+            .replace("eight", "eight8eight")
+            .replace("nine", "nine9nine")
+            .chars().filter_map(|num| num.to_digit(10)).collect();
+        values.push(numbers);
+    }
 
-    281
+    let sum = calculate_sum(values);
+    println!("{sum}");
+    sum
+}
+
+fn calculate_sum(values: Vec<Vec<u32>>) -> u32 {
+    let mut sum: u32 = 0;
+
+    for numbers in values {
+        let digit = numbers[0].to_string() + &*numbers[numbers.len() - 1].to_string();
+        sum += digit.parse::<u32>().unwrap();
+    }
+    sum
 }
