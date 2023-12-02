3471667afba0f26ec5133ccc9029f53e756013c0
legoraft <50689727+legoraft@users.noreply.github.com>
2023-12-02 10:09:21 +0100

    Added day 2 structure

---
+++ 2023/src/days/day_02.rs
@@ -0,0 +1,10 @@
+pub fn main() {
+    let games = include_str!("../inputs/day_01.txt");
+    let answer: Vec<_> = vec!["Hello", "World"];
+
+    println!("-- Day One --\nPart 1: {:?}\nPart 2: {:?}\n", answer[0], answer[1])
+}
+
+pub fn part_one(games: &str) {
+
+}
--- 2023/src/tests.rs
+++ 2023/src/tests.rs
@@ -23,3 +23,16 @@ zoneight234
     assert_eq!(142, day_01::part_one(calibration_value));
     assert_eq!(281, day_01::part_two(calibration_value_text));
 }
+
+#[test]
+fn day_02() {
+    let games = "
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+";
+
+    assert_eq!(8, day_02::part_one(games))
+}
