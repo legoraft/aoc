c33bc889087cc3c2f1309973584f97087e4840b9
legoraft <50689727+legoraft@users.noreply.github.com>
2023-12-04 16:26:22 +0100

    splitted out some stuff

--- 2023/src/days/day_04.rs
+++ 2023/src/days/day_04.rs
@@ -1,6 +1,6 @@
 pub fn main() {
     let cards = include_str!("../inputs/day_04.txt");
-    let answer: Vec<u32> = vec![part_one(cards), 0];
+    let answer: Vec<u32> = vec![part_one(cards), part_two(cards)];
 
     println!("-- Day Four --\nPart 1: {:?}\nPart 2: {:?}\n", answer[0], answer[1]);
 }
@@ -10,25 +10,49 @@ pub fn part_one(cards: &str) -> u32 {
     let mut total_score: u32 = 0;
 
     for card in cards {
-        let card = &card[10..];
-        let card: Vec<&str> = card.split("|").collect();
-        let mut score = 0;
-
-        let winning_numbers: Vec<&str> = card[0].trim().split(" ").collect();
-        let mut my_numbers: Vec<&str> = card[1].trim().split(" ").collect();
-        my_numbers.retain(|value| *value != "");
+        let mut score: u32 = 0;
+        let (winning_numbers, my_numbers) = process_cards(card);
 
         for number in my_numbers {
-            let test = winning_numbers.contains(&number);
+            let win = winning_numbers.contains(&number);
 
-            if test && score == 0 {
+            if win && score == 0 {
                 score += 1;
-            } else if test {
+            } else if win {
                 score += score;
             }
         }
-        dbg!(score);
         total_score += score;
     }
+
     total_score
+}
+
+pub fn part_two(cards: &str) -> u32 {
+    let cards: Vec<&str> = cards.lines().collect();
+    let mut total_score: u32 = 0;
+
+    for card in cards {
+        let mut score: u32 = 0;
+        let (winning_numbers, my_numbers) = process_cards(card);
+
+        for number in my_numbers {
+            let win = winning_numbers.contains(&number);
+
+        }
+        total_score += score;
+    }
+
+    total_score
+}
+
+fn process_cards(card: &str) -> (Vec<&str>, Vec<&str>) {
+    let card = &card[10..];
+    let card: Vec<&str> = card.split("|").collect();
+
+    let winning_numbers: Vec<&str> = card[0].trim().split(" ").collect();
+    let mut my_numbers: Vec<&str> = card[1].trim().split(" ").collect();
+    my_numbers.retain(|value| *value != "");
+
+    (winning_numbers, my_numbers)
 }
