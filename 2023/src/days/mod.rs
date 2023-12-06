be3086aa96f9f67c81f39c00a56acc2d534d81d9
legoraft <50689727+legoraft@users.noreply.github.com>
2023-12-06 17:31:56 +0100

    Made executing all days easier

--- 2023/src/days/mod.rs
+++ 2023/src/days/mod.rs
@@ -3,3 +3,11 @@ pub mod day_02;
 pub mod day_04;
 pub mod day_05;
 pub mod day_06;
+
+pub fn main() {
+    day_01::main();
+    day_02::main();
+    day_04::main();
+    day_05::main();
+    day_06::main();
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,9 +1,5 @@
 use aoc_2023::days;
 
 fn main() {
-    days::day_01::main();
-    days::day_02::main();
-    days::day_04::main();
-    days::day_05::main();
-    days::day_06::main();
+    days::main();
 }
