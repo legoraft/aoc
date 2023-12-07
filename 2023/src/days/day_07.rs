fd609bcf999e834bba79227596e847667f83d385
legoraft <50689727+legoraft@users.noreply.github.com>
2023-12-07 12:03:33 +0100

    Added poker parser

--- 2023/src/days/day_07.rs
+++ 2023/src/days/day_07.rs
@@ -1,53 +1,109 @@
+use std::collections::HashSet;
+use std::thread::panicking;
 use crate::output_part;
 
 pub fn main() {
-    let hands = include_str!("../inputs/day_07.txt");
+    let games = include_str!("../inputs/day_07.txt");
 
-    println!("{}", output_part(|| part_one(hands), || part_two(hands), "07"))
+    println!("{}", output_part(|| part_one(games), || part_two(games), "07"))
 }
 
 #[derive(Debug)]
 struct Hand {
     bid: i64,
-    cards: Vec<char>,
+    cards: HashSet<Card>,
     values: Vec<i64>,
+    rank: i64,
 }
 
-pub fn part_one(hands: &str) -> i64 {
-    let games = parse(hands);
-    dbg!(games);
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct Card {
+    id: char,
+    amount: i64,
+}
+
+pub fn part_one(games: &str) -> i64 {
+    let games: Vec<Hand> = parse(games);
+    for game in games {
+        // combos are numbered as follows:
+        // 1 = high card
+        // 2 = one pair
+        // 3 = two pair
+        // 4 = three of a kind
+        // 5 = full house
+        // 6 = four of a kind
+        // 7 = five of a kind
+        let mut combo = 0;
+        let mut nothing = 0;
+        let mut pairs = 0;
+        let mut threes = 0;
+
+        for card in &game.cards {
+            match card.amount {
+                1 => nothing += 1,
+                2 => pairs += 1,
+                3 => threes += 1,
+                4 => combo = 6,
+                5 => combo = 7,
+                _ => println!("Prob something wrong...")
+            }
+        }
+
+        if pairs == 1 && threes == 1 {
+            combo = 5;
+        } else if pairs == 2 {
+            combo = 3;
+        } else if nothing == game.cards.len() + 1 {
+            combo = 1;
+        } else {
+            combo = threes * 4 + pairs * 2;
+        }
+    }
+
     0
 }
 
-pub fn part_two(hands: &str) -> i64 {
+pub fn part_two(games: &str) -> i64 {
     0
 }
 
-fn parse(hands: &str) -> Vec<Hand> {
-    let hands: Vec<&str> = hands.lines().collect();
-    let mut games: Vec<Hand> = Vec::new();
+fn parse(games: &str) -> Vec<Hand> {
+    let games: Vec<&str> = games.lines().collect();
+    let mut rounds: Vec<Hand> = Vec::new();
 
-    for hand in hands {
-        let (cards, bid) = hand.split_once(" ").unwrap();
+    for game in games {
+        let (hand, bid) = game.split_once(" ").unwrap();
+        let mut cards: Vec<Card> = Vec::new();
 
         let bid: i64 = bid.parse().unwrap();
-        let cards: Vec<char> = cards.chars().collect();
+        let hand: Vec<char> = hand.chars().collect();
 
-        let values: Vec<String> = cards.iter().map(|char| char.to_string()).collect();
+        let values: Vec<String> = hand.iter().map(|char| char.to_string()).collect();
         let values: Vec<String> = values.iter().map(|card| card
-                .replace("A", "14")
-                .replace("K", "13")
-                .replace("Q", "12")
-                .replace("J", "11")
-                .replace("T", "10")
+            .replace("A", "14")
+            .replace("K", "13")
+            .replace("Q", "12")
+            .replace("J", "11")
+            .replace("T", "10")
         ).collect();
 
-        games.push(Hand{
+        for card in &hand {
+            let card_count = hand.iter().filter(|count| *count == card).count();
+            cards.push(Card {
+                id: *card,
+                amount: card_count as i64,
+            });
+        }
+
+        let cards: HashSet<Card> = cards.drain(..).collect();
+
+        rounds.push(Hand{
             bid,
             cards,
             values: values.iter().map(|s| s.parse::<i64>().unwrap()).collect(),
+            rank: 1,
         })
     }
 
-    games
+    rounds
 }
