4689ed9f43c4ad4ed23246befecf759627a6fde3
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-07 19:23:18 +0200

    Fully finished part one

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    println!("Hello, first day!");
-    let input_file: &str = "\
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet";
-    part_one(input_file);
+    let input_file = include_str!("../../inputs/day_01.txt");
+
+    let answer_one = part_one(input_file);
+
+    println!("Part one: {}\n", answer_one);
 }
 
 fn part_one(input: &str) -> i64 {
