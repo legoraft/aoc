a55ca4b32f270eff43f24709276d382e01e8059f
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-07 18:36:47 +0200

    Added day 1 part 1 test

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, first day!");
 }
+
+fn part_one(file: &str) {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    fn test_part_one() {
+        let input_file: &str = "\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+
+        let answer: i64 = 142
+
+        assert_eq!(answer, part_one(input_file));
+    }
+}
