8cde6fd5c0e6a804a1d02d22d70aad0e506947b4
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-07 19:04:58 +0200

    Parsing all numbers

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -1,11 +1,26 @@
 fn main() {
     println!("Hello, first day!");
+    let input_file: &str = "\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+    part_one(input_file);
 }
 
-fn part_one(lines: &str) {
+fn part_one(input: &str) -> i64 {
+    let lines = parse(input);
+
     for line in lines {
+        let nums: Vec<u32> = line.chars()
+            .filter(|c| c.is_digit(10))
+            .map(|c| c.to_digit(10).expect("Can't parse character!"))
+            .collect();
         
+
     }
+
+    1
 }
 
 fn parse(file: &str) -> Vec<&str> {
@@ -16,14 +31,17 @@ fn parse(file: &str) -> Vec<&str> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    
+    #[test]
     fn test_part_one() {
         let input_file: &str = "\
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet"
+treb7uchet";
 
-        let answer: i64 = 142
+        let answer: i64 = 142;
 
         assert_eq!(answer, part_one(input_file));
     }
