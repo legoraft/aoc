34d03e8d78c0544baddcc9f5b11624fb6a7088ee
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-08 23:03:43 +0200

    Added test for day 3

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -9,3 +9,27 @@ fn main() {
 fn part_one(input: &str) -> i64 {
     0
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input_file: &str = "\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+        let answer: i64 = 4361;
+
+        assert_eq!(answer, part_one(input_file));
+    }
+}
