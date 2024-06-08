a9353fb0f7f53ac97ce5678713bca43e27267162
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-08 21:14:19 +0200

    Added simple parser

--- 2023/day_02/src/main.rs
+++ 2023/day_02/src/main.rs
@@ -1,11 +1,29 @@
 fn main() {
-    println!("Hello, day 2!");
+    let input_file: &str = "\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    println!("{:?}", parse(input_file));
 }
 
 fn part_one(input: &str) -> i64 {
 
 
     8
+}
+
+fn parse(file: &str) -> Vec<&str> {
+    let lines: Vec<&str> = file.lines()
+        .map(|l| { 
+            let (_, cubes) = l
+                .split_once(": ")
+                .expect("Couldn't split line!"); cubes })
+        .collect();
+
+    lines
 }
 
 #[cfg(test)]
