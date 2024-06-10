c8323d2166797c16200c7a2174426d7344ce0f00
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-10 16:45:38 +0200

    Added struct for partnumbers

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -1,9 +1,34 @@
+use std::collections::HashSet;
+
 fn main() {
     let input_file = include_str!("../../../2023/inputs/day_03.txt");
 
     let answer_one = part_one(input_file);
 
     println!("Part one: {}\n", answer_one);
+}
+
+struct PartNumber {
+    value: i64,
+    points: HashSet<(i64, i64)>,
+}
+
+impl PartNumber {
+    fn new(x: i64, y: i64, ch: char) -> Self {
+        let points = HashSet::from([
+            (x - 1, y -1), (x, y -1), (x + 1, y - 1),
+            (x - 1, y), (x + 1, y),
+            (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+        ]);
+        Self {
+            value: ch as i64,
+            points,
+        }
+    }
+    fn add_digit(&mut self, x: i64, y: i64, ch: char) {
+        self.value = self.value * 10 + ch as i64;
+        self.points.extend([ (x - 1, y + 1), ((x, y + 1)), (x + 1, y + 1) ]);
+    }
 }
 
 fn part_one(input: &str) -> i64 {
@@ -22,9 +47,7 @@ fn part_one(input: &str) -> i64 {
             })
         }).collect();
 
-    for char in chars {
-        
-    }
+    
 
     0
 }
