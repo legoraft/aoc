4c46558eb22755ab2e2c606b32c16c3506b665a8
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-10 17:11:18 +0200

    FInished most of solution

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("Part one: {}\n", answer_one);
 }
 
+#[derive(Debug)]
 struct PartNumber {
     value: i64,
     points: HashSet<(i64, i64)>,
@@ -16,40 +17,54 @@ struct PartNumber {
 impl PartNumber {
     fn new(x: i64, y: i64, ch: char) -> Self {
         let points = HashSet::from([
-            (x - 1, y -1), (x, y -1), (x + 1, y - 1),
-            (x - 1, y), (x + 1, y),
-            (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+            (x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+            (x, y - 1), (x, y + 1),
+            (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)
         ]);
         Self {
-            value: ch as i64,
+            value: (ch as u8 - b'0') as i64,
             points,
         }
     }
     fn add_digit(&mut self, x: i64, y: i64, ch: char) {
-        self.value = self.value * 10 + ch as i64;
-        self.points.extend([ (x - 1, y + 1), ((x, y + 1)), (x + 1, y + 1) ]);
+        self.value = self.value * 10 + (ch as u8 - b'0') as i64;
+        self.points.extend([ (x + 1, y - 1), ((x + 1, y)), (x + 1, y + 1) ]);
     }
 }
 
 fn part_one(input: &str) -> i64 {
     let map = parser(input);
 
-    let chars: Vec<(usize, usize)> = map
-        .iter()
-        .enumerate()
-        .flat_map(|(y, row)| {
-            row.iter()
-            .enumerate()
-            .filter_map(move |(x, &ch)| if !ch.is_digit(10) && ch != '.' {
-                Some((x, y))
+    let mut nums: Vec<PartNumber> = Vec::new();
+    let mut syms: HashSet<(i64, i64)> = HashSet::new();
+
+    let mut cur_number: Option<PartNumber> = None;
+
+    for (y, lines) in map.iter().enumerate() {
+        for (x, &ch) in lines.iter().enumerate() {
+            if ch.is_digit(10) {
+                if let Some(ref mut num) = cur_number {
+                    num.add_digit(x as i64, y as i64, ch);
+                } else {
+                    cur_number = Some(PartNumber::new(x as i64, y as i64, ch));
+                }
             } else {
-                None
-            })
-        }).collect();
+                if let Some(num ) = cur_number.take() {
+                    nums.push(num);
+                } if ch != '.' {
+                    syms.insert((x as i64, y as i64));
+                }
+            }
+        }
+    }
 
-    
+    let total: i64 = nums
+        .iter()
+        .filter(|num| num.points.intersection(&syms).next().is_some())
+        .map(|num| num.value)
+        .sum::<i64>();
 
-    0
+    total
 }
 
 fn parser(file: &str) -> Vec<Vec<char>> {
