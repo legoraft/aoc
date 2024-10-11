f6bc9a47bba5a85c0db0381869bf43d7d7cc7bac
legoraft <legoraft@legoraft.com>
2024-10-11 08:56:57 +0200

    Added testing for day 06

--- 2023/day_06/src/main.rs
+++ 2023/day_06/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_part_one() {
+        let input_file: &str = "\
+Time:      7  15   30
+Distance:  9  40  200";
+        
+        let answer: i64 = 288;
+
+        assert_eq!(answer, part_one(input_file));
+}
