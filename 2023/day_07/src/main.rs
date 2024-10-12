6c59145abda5d39558faa5552a0304f558d05389
legoraft <legoraft@legoraft.com>
2024-10-12 17:25:45 +0200

    feat: added part 2 solver

--- 2023/day_07/src/main.rs
+++ 2023/day_07/src/main.rs
@@ -18,6 +18,7 @@ struct Hand {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Card {
+    Joker,
     Two,
     Three,
     Four,
@@ -34,7 +35,7 @@ enum Card {
 }
 
 impl Card {
-    fn from_char(card: char) -> Self {
+    fn from_char(card: char, joker: bool) -> Self {
         match card {
             '2' => Self::Two,
             '3' => Self::Three,
@@ -45,7 +46,7 @@ impl Card {
             '8' => Self::Eight,
             '9' => Self::Nine,
             'T' => Self::Ten,
-            'J' => Self::Jack,
+            'J' => if joker { Self::Joker } else { Self::Jack },
             'Q' => Self::Queen,
             'K' => Self::King,
             'A' => Self::Ace,
@@ -55,7 +56,7 @@ impl Card {
 }
 
 fn part_one(file: &str) -> i64 {
-    let games = parse(file);
+    let games = parse(file, false);
     let mut hands: Vec<Hand> = Vec::new();
     
     for mut game in games {
@@ -99,16 +100,61 @@ fn part_one(file: &str) -> i64 {
 }
 
 fn part_two(file: &str) -> i64 {
-    0
+    let games = parse(file, true);
+    
+    let mut hands: Vec<Hand> = Vec::new();
+    
+    for mut game in games {
+        let mut score: HashMap<&Card, i64> = HashMap::new();
+        
+        for card in &game.cards {
+            score.entry(card)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+        }
+        
+        let jokers: i64 = if score.contains_key(&Card::Joker) {
+            score[&Card::Joker]
+        } else { 0 };
+        
+        let mut values: Vec<i64> = score.values().cloned().collect();
+        values.sort();
+        values.reverse();
+        values.push(0);
+        
+        let score: [i64; 2] = [values[0] + jokers, values[1]];
+        game.score = score;
+        
+        hands.push(game);
+    }
+    
+    hands.sort_by(|a, b| {
+        let score_cmp = a.score.cmp(&b.score);
+        
+        if score_cmp == std::cmp::Ordering::Equal {
+            a.cards.cmp(&b.cards)
+        } else {
+            score_cmp
+        }
+    });
+    
+    let mut answer = 0;
+    
+    for (rank, hand) in hands.iter().enumerate() {
+        let score = hand.bid * (rank as i64 + 1);
+        answer += score;
+    }
+    
+    answer
 }
 
-fn parse(file: &str) -> Vec<Hand> {
+fn parse(file: &str, joker: bool) -> Vec<Hand> {
     let hands: Vec<(&str, &str)> = file.lines().map(|l| l.split_once(" ").expect("Couldn't split line!")).collect();
     let mut games: Vec<Hand> = Vec::new();
     
     for (cards, bid) in hands {
         let bid: i64 = bid.parse().expect("Couldn't parse bid!");
-        let cards: Vec<Card> = cards.chars().map(|c| Card::from_char(c)).collect();
+        let cards: Vec<Card> = cards.chars().map(|c| Card::from_char(c, joker)).collect();
         
         games.push(Hand {
             cards,
