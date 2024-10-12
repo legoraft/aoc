987266865c311f729533b0d8a162d9c76148db2e
legoraft <legoraft@legoraft.com>
2024-10-12 18:35:13 +0200

    chore: added boilerplate in day 8

--- 2023/day_08/src/main.rs
+++ 2023/day_08/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    let input_file = include_str!("../../inputs/day_08.txt");
+
+    let answer_one = part_one(input_file);
+//    let answer_two = part_two(input_file);
+
+    println!("Part one: {}\nPart two: {}", answer_one, /* answer_two */);
+}
+
+fn part_one(file: &str) -> i64 {
+    0
+}
+
+fn parse(file: &str) {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    const INPUT: &str = "\
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+    
+    #[test]
+    fn test_part_one() {
+        let answer: i64 = 6;
+
+        assert_eq!(answer, part_one(INPUT));
+    }
 }
