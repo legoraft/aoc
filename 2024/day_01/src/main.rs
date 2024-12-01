8102ad03b6b3c272663996ebbf70bfac28ca3202
legoraft <git@legoraft.com>
2024-12-01 09:50:57 +0100

    feat: solved part two, refactored tests

--- 2024/day_01/src/main.rs
+++ 2024/day_01/src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     let input_file = include_str!("../../inputs/day_01.txt");
     
     let answer_one = part_one(input_file);
+    let answer_two = part_two(input_file);
     
-    println!("Part one: {}", answer_one);
+    println!("Part one: {}\nPart two: {}", answer_one, answer_two);
 }
 
 fn part_one(input: &str) -> i64 {
@@ -22,6 +23,19 @@ fn part_one(input: &str) -> i64 {
     answer
 }
 
+fn part_two(input: &str) -> i64 {
+    let (left_list, right_list) = parse(input);
+    let mut answer: i64 = 0;
+    
+    for item in left_list {
+        let count = right_list.iter().filter(|&n| *n == item).count();
+        
+        answer += item * count as i64;
+    }
+    
+    answer
+}
+
 fn parse(file:  &str) -> (Vec<i64>, Vec<i64>) {
     let input: Vec<i64> = file.split_whitespace().map(|n| n.parse().expect("Couldn't parse number!")).collect();
     
@@ -34,10 +48,8 @@ fn parse(file:  &str) -> (Vec<i64>, Vec<i64>) {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let input = "\
+    
+    const INPUT: &str = "\
 3   4
 4   3
 2   5
@@ -45,8 +57,17 @@ mod tests {
 3   9
 3   3";
 
+    #[test]
+    fn test_part_one() {
         let answer = 11;
 
-        assert_eq!(answer, part_one(input))
+        assert_eq!(answer, part_one(INPUT))
+    }
+    
+    #[test]
+    fn test_part_two() {
+        let answer = 31;
+
+        assert_eq!(answer, part_two(INPUT))
     }
 }
