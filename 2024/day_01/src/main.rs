97a0615351a5fa389343f44679b31b3852844e20
legoraft <git@legoraft.com>
2024-12-01 09:45:10 +0100

    feat: solved part one day 1

--- 2024/day_01/src/main.rs
+++ 2024/day_01/src/main.rs
@@ -7,14 +7,28 @@ fn main() {
 }
 
 fn part_one(input: &str) -> i64 {
-    parse(input);
+    let (mut list_one, mut list_two) = parse(input);
+    let mut answer: i64 = 0;
     
-    0
+    list_one.sort();
+    list_two.sort();
+    
+    for (index, item) in list_one.iter().enumerate() {
+        let difference = item - list_two[index];
+        
+        answer += difference.abs();
+    }
+    
+    answer
 }
 
-fn parse(file:  &str) {
-    let input: Vec<&str> = file.split_whitespace().collect();
-    dbg!(input);
+fn parse(file:  &str) -> (Vec<i64>, Vec<i64>) {
+    let input: Vec<i64> = file.split_whitespace().map(|n| n.parse().expect("Couldn't parse number!")).collect();
+    
+    let list_one: Vec<i64> = input.clone().into_iter().step_by(2).collect();
+    let list_two: Vec<i64> = input.clone().into_iter().skip(1).step_by(2).collect();
+
+    (list_one, list_two)
 }
 
 #[cfg(test)]
